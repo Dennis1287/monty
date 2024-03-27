af234f6209e9cd90d3642360a4e2c6c303d9f434
Dennis Miringu <dennismiringu03@gmail.com>
2024-03-26 23:47:09 -0700

    push change

--- opcodes_1.c
+++ opcodes_1.c
@@ -1,7 +1,9 @@
 #include "monty.h"
 
 void push(stack_t **stack, int value) {
-    stack_t *new_node = malloc(sizeof(stack_t));
+    stack_t *new_node;
+
+    new_node = malloc(sizeof(stack_t));
     if (new_node == NULL) {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
